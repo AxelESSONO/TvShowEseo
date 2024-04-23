9db6eb9d4116cb6247ad3e7694fb6211a3fea078
axelessono <naxelcdric@gmail.com>
2024-04-23 11:49:47 +0200

    Initial commit

---
+++ app/src/main/java/com/eseo/tvshow/model/remote/TvShowResponse.kt
@@ -0,0 +1,18 @@
+package com.eseo.tvshow.model.remote
+
+
+import com.google.gson.annotations.SerializedName
+
+data class TvShowResponse(
+    //@SerializedName("page")
+    val page: Int,
+
+    //@SerializedName("pages")
+    val pages: Int,
+
+    //@SerializedName("total")
+    val total: String,
+
+    @SerializedName("tv_shows")
+    val tvShows: List<TvShow>
+)
---
+++ app/src/main/java/com/eseo/tvshow/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.eseo.tvshow.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eseo.tvshow.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
